547e0e13be19e7a98344be85aeed0dafa750eacd
e <nieyangyang712@163.com>
2021-03-30 20:44:17 +0800

    update

--- xiaoyiman/static/js/chunk-2d0abc7d.7c745c65.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0abc7d"],{1778:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" 账号管理 ")])},u=[],l={},a=l,r=t("2877"),s=Object(r["a"])(a,c,u,!1,null,null,null);e["default"]=s.exports}}]);
--- xiaoyiman/static/js/chunk-2d218437.8aab767a.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d218437"],{c9f1:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" 设备巡检 ")])},u=[],l={},r=l,s=t("2877"),a=Object(s["a"])(r,c,u,!1,null,null,null);e["default"]=a.exports}}]);
